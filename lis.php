cd4c4b7a6c9675722f3cfbfe3647e6c202e297ac
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-09-23 15:34:18 -0300

    Initial work with public html templates

---
+++ lis.php
@@ -0,0 +1,101 @@
+<?php
+/*
+Plugin Name: LIS
+Plugin URI: http://reddes.bvsalud.org/projects/lis/
+Description: Create a Health Information Locator Site
+Author: BIREME/OPAS/OMS
+Version: 0.1
+Author URI: http://reddes.bvsalud.org/
+*/
+
+define('LIS_VERSION', '0.1' );
+
+define('LIS_SYMBOLIC_LINK', false );
+define('LIS_PLUGIN_DIRNAME', 'lis-page' );
+
+if(LIS_SYMBOLIC_LINK == true) {
+    define('LIS_PLUGIN_PATH',  ABSPATH . 'wp-content/plugins/' . LIS_PLUGIN_DIRNAME );
+} else {
+    define('LIS_PLUGIN_PATH',  plugin_dir_path(__FILE__) );
+}
+
+define('LIS_PLUGIN_DIR',   plugin_basename( LIS_PLUGIN_PATH ) );
+define('LIS_PLUGIN_URL',   plugin_dir_url(__FILE__) );
+
+function lis_theme_redirect() {
+    global $wp;
+    $pagename = $wp->query_vars["pagename"];
+
+    if ($pagename == 'lis' || $pagename == 'lis/resource') {
+        add_action( 'wp_enqueue_scripts', 'page_template_styles_scripts' );
+
+        if ($pagename == 'lis'){
+            $template = LIS_PLUGIN_PATH . '/template/lis-home.php';
+        }else{
+            $template = LIS_PLUGIN_PATH . '/template/lis-resource.php';
+        }
+        // redirect to page and finish execution
+        include($template);
+        die();
+    }
+}
+
+function page_template_styles_scripts(){
+    wp_enqueue_script('lis-page',    LIS_PLUGIN_URL . 'template/js/functions.js');
+    wp_enqueue_script('jquery-raty', LIS_PLUGIN_URL . 'template/js/jquery.raty.min.js', array( 'jquery' ));
+    wp_enqueue_style ('lis-page',    LIS_PLUGIN_URL . 'template/css/style.css');
+}
+
+function lis_init() {
+
+}
+
+function lis_load_translation(){
+    // Translations
+    load_plugin_textdomain( 'lis', false,  LIS_PLUGIN_DIR . '/languages' );
+}
+
+function lis_add_admin_menu() {
+
+    add_submenu_page( 'options-general.php', __('LIS Page Settings', 'lis'), __('LIS Page', 'lis'), 'manage_options', 'lis',
+                      'lis_page_admin');
+
+    //call register settings function
+    add_action( 'admin_init', 'lis_register_settings' );
+
+}
+
+function lis_register_settings(){
+
+    register_setting('lis-settings-group', 'lis_config');
+
+}
+
+function lis_google_analytics_code(){
+    $lis_config = get_option('lis_config');
+    if ($lis_config['google_analytics_code'] != ''){
+?>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo $lis_config['google_analytics_code'] ?>']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<?php
+    } //endif
+}
+
+add_action( 'init', 'lis_load_translation' );
+add_action( 'admin_menu', 'lis_add_admin_menu');
+add_action( 'plugins_loaded','lis_init' );
+add_action( 'wp_head', 'lis_google_analytics_code');
+add_action( 'template_redirect', 'lis_theme_redirect');
+?>
---
+++ template/lis-resource.php
@@ -0,0 +1,185 @@
+<?php
+/*
+Template Name: LIS Detail
+*/
+
+$request_uri = $_SERVER["REQUEST_URI"];
+$request_parts = explode('/', $request_uri);
+$resource_id = end($request_parts);
+
+?>
+
+
+<?php get_header(); ?>
+
+<div id="content" class="row-fluid">
+        <div class="ajusta2">
+            <div class="row-fluid">
+                <section class="header-search">
+                    <form role="search" method="get" id="searchform" action="http://localhost/lis">
+                        <input value="" name="s" class="input-search" id="s" type="text" placeholder="Pesquisar...">
+                        <input id="searchsubmit" value="" type="submit" class="b-search">
+                    </form>
+                </section>
+
+                <div class="pull-right">
+                    <a href="enviar-colaboracion" class="header-colabore">Indique um site</a>
+                </div>
+            </div>
+
+            <section id="conteudo">
+                <header class="row-fluid border-bottom">
+                    <h1 class="h1-header">Más Recientes</h1>
+                    <div class="pull-right">
+                        <a href="#" class="ico-feeds"></a>
+                        <form action="">
+                            <select name="txtRegistros" id="txtRegistros" class="select-input-home">
+                                <option value="10 Registros">10 registros</option>
+                                <option value="20 Registros">20 registros</option>
+                                <option value="50 Registros">50 registros</option>
+                            </select>
+
+                            <select name="txtOrder" id="txtOrder" class="select-input-home">
+                                <option value="">Ordenar por</option>
+                                <option value="Mais Recentes">Mais Recentes</option>
+                                <option value="Mais Lidas">Mais Lidas</option>
+                            </select>
+                        </form>
+                    </div>
+                </header>
+                <div class="row-fluid">
+                    <article class="conteudo-loop">
+                        <div class="row-fluid">
+                            <h2 class="h2-loop-tit">Estratégia governamental para internalização de fármacos e medicamentos em doenças negligenciadas</h2>
+                        </div>
+                            <div class="conteudo-loop-rates">
+                                <div class="star" data-score="1"></div>
+                            </div>
+                            <span class="row-fluid margintop05">
+                                <a href="http://tpqb.eq.ufrj.br/download/farmacos-e-medicamentos-em-doencas-negligenciadas.pdf">http://tpqb.eq.ufrj.br/download/farmacos-e-medicamentos-em-doencas-negligenciadas.pdf</a>   
+                            </span>
+                            <p class="row-fluid">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet neque ut massa egestas accumsan. Mauris bibendum nibh non metus gravida, vehicula tristique odio tincidunt. Nunc at sem vel nisl semper laoreet. Donec lacinia, massa eget fermentum aliquam, est leo ultricies urna, nec pharetra diam nibh eu sapien. Duis vel risus auctor, posuere odio nec, egestas ipsum. Mauris eget est quam. Donec in nunc quis nisl feugiat fringilla.</p>
+
+                            <div id="conteudo-loop-data" class="row-fluid margintop05">
+                                <span class="conteudo-loop-data-tit">Sugerido em:</span>
+                                29/07/2013 - 5h33                           
+                            </div>
+
+                            <div id="conteudo-loop-idiomas" class="row-fluid">
+                                <span class="conteudo-loop-idiomas-tit">Idiomas disponíveis:</span>
+                                Português, English, Español
+                            </div>
+
+                            <div id="conteudo-loop-tags" class="row-fluid margintop10">
+                                <i class="ico-tags"></i>
+                                <a href="#" rel="tag">Chagas</a>, <a href="#" rel="tag">Dengue</a>, <a href="#" rel="tag">Hanseníase</a>, <a href="#" rel="tag">Leishmaniose</a>, <a href="#" rel="tag">Malária</a><br />
+                            </div>
+
+                            <footer class="row-fluid margintop5">
+                                <ul class="conteudo-loop-icons">
+                                    <li class="conteudo-loop-icons-li">
+                                        <a href="#">
+                                            <i class="ico-compartilhar"></i>
+                                            Compartilhar
+                                        </a>
+                                    </li>
+
+                                    <li class="conteudo-loop-icons-li">
+                                        <a href="#">
+                                            <i class="ico-tag"></i>
+                                            Sugerir Tag
+                                        </a>
+                                    </li>
+
+                                    <li class="conteudo-loop-icons-li">
+                                        <span class="reportar-erro-open">
+                                            <i class="ico-reportar"></i>
+                                            Reportar Erro
+                                        </span>
+
+                                        <div class="reportar-erro"> 
+                                            <form action="">
+                                                <div class="reportar-erro-close">[X]</div>
+                                                <span class="reportar-erro-tit">Motivo</span>
+
+                                                <div class="row-fluid margintop05">
+                                                    <input type="radio" name="txtMotivo" id="txtMotivo1">
+                                                    <label class="reportar-erro-lbl" for="txtMotivo1">Motivo 01</label>
+                                                </div>
+
+                                                <div class="row-fluid">
+                                                    <input type="radio" name="txtMotivo" id="txtMotivo2">
+                                                    <label class="reportar-erro-lbl" for="txtMotivo2">Motivo 02</label>
+                                                </div>
+
+                                                <div class="row-fluid">
+                                                    <input type="radio" name="txtMotivo" id="txtMotivo3">
+                                                    <label class="reportar-erro-lbl" for="txtMotivo3">Motivo 03</label>
+                                                </div>
+
+                                                <div class="row-fluid margintop05">
+                                                    <textarea name="txtArea" id="txtArea" class="reportar-erro-area" cols="20" rows="2"></textarea>
+                                                </div>
+
+                                                <div class="row-fluid border-bottom2"></div>
+
+                                                <span class="reportar-erro-tit margintop05">Nueva URL (Opcional)</span>
+                                                <div class="row-fluid margintop05">
+                                                    <textarea name="txtUrl" id="txtUrl" class="reportar-erro-area" cols="20" rows="2"></textarea>
+                                                </div>
+
+                                                <div class="row-fluid border-bottom2"></div>
+
+                                                <div class="row-fluid margintop05">
+                                                    <button class="pull-right reportar-erro-btn">Enviar</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </li>
+
+                                    <li class="conteudo-loop-icons-li">
+                                        <a href="#">
+                                            <i class="ico-comentar"></i>
+                                            Comentar
+                                        </a>
+                                    </li>
+                                </ul>
+                            </footer>
+                        </article>
+                    </div>
+
+            </section>
+
+            <aside id="sidebar">
+                <section class="row-fluid marginbottom25 widget_categories">
+                    <header class="row-fluid border-bottom marginbottom15">
+                        <h1 class="h1-header">Categorias</h1>
+                    </header>
+                    <ul>
+                        <li class="cat-item"><a href="http://localhost/lis/category/educacao/">Educação</a><span class="cat-item-count">3</span>
+                        </li>
+                        <li class="cat-item"><a href="http://localhost/lis/category/gestao-em-saude/">Gestão em Saúde</a><span class="cat-item-count">1</span>
+                        </li>
+                        <li class="cat-item"><a href="http://localhost/lis/category/politica/">Política</a><span class="cat-item-count">3</span>
+                        </li>
+                        <li class="cat-item"><a href="http://localhost/lis/category/saude/">Saúde</a><span class="cat-item-count">2</span>
+                        </li>
+                        <li class="cat-item"><a href="http://localhost/lis/category/servicos-de-saude/">Serviços de Saúde</a><span class="cat-item-count">1</span>
+                        </li>
+                    </ul>
+                </section>
+            </aside>
+
+        </div>
+    </div>
+    <script>
+
+        $('.star').raty({
+            path: '/lis/wp-content/themes/lis/Js/raty-2.5.2/lib/img/',
+            score: function() {
+            return $(this).attr('data-score');
+          }
+        });
+    </script>
+
+<?php get_footer();?>
