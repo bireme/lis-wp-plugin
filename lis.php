c51b9399d2e3c4968a9a9c8b9f2a3bf8700edde8
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-10-21 12:16:25 -0200

    Separate file for template functions

--- lis.php
+++ lis.php
@@ -25,6 +25,7 @@ define('LIS_PLUGIN_URL',   plugin_dir_url(__FILE__) );
 $plugin_slug = 'lis';
 
 require_once(LIS_PLUGIN_PATH . '/settings.php');
+require_once(LIS_PLUGIN_PATH . '/template-functions.php');
 
 function lis_theme_redirect() {
     global $wp, $plugin_slug;
@@ -137,30 +138,6 @@ function lis_page_title(){
     return 'LIS | ';
 }    
 
-function print_lang_value($value, $lang_code){
-    if ( is_array($value) ){
-        foreach($value as $current_value){
-            $print_values[] = get_lang_value($current_value, $lang_code);
-        }
-        echo implode(', ', $print_values);
-    }else{
-        echo get_lang_value($current_value, $lang_code);
-    }
-    return;
-}
-
-function get_lang_value($string, $lang_code){
-    $lang_value = array();
-    $occs = preg_split('/\|/', $string);
-    
-    foreach ($occs as $occ){
-        $lv = preg_split('/\^/', $occ);
-        $lang = $lv[0];
-        $value = $lv[1];        
-        $lang_value[$lang] = $value;        
-    }
-    return $lang_value[$lang_code];
-}
 
 add_action( 'init', 'lis_load_translation' );
 add_action( 'admin_menu', 'lis_add_admin_menu');
