77143ac51cbe78b3ab947dd5ab0a4537fad7d3dc
wilsonmoura <wilsonsmoura@gmail.com>
2022-01-31 17:34:28 -0300

    Update Paginator.php

--- lib/Paginator.php
+++ lib/Paginator.php
@@ -5,15 +5,15 @@ class Paginator{
     var $start;
     var $output;
     var $pag = array();
-    var $default_count = 20;
+    var $default_count = 10;
     var $current_page;
 
     function __construct($total, $start)
     {
         $this->total = $total;
         $this->start = $start;
-        $this->count = ( !empty($_GET['count']) ? $_GET['count'] : $this->default_count );
-        $this->current_page = ( !empty($_GET['pg']) ? $_GET['pg'] : 1);
+        $this->count = ( !empty($_GET['count'] ) ? $_GET['count'] : $this->default_count );
+        $this->current_page = ( !empty($_GET['pg'] ) ? $_GET['pg'] : 1);
     }
 
     function paginate($page_url_params){
