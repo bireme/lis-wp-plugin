149c7591b883d73260996015a7abd974d19b3e5b
wilsonmoura <wilsonsmoura@gmail.com>
2022-01-31 10:20:05 -0300

    Update Paginator.php

--- lib/Paginator.php
+++ lib/Paginator.php
@@ -8,7 +8,7 @@ class Paginator{
     var $default_count = 20;
     var $current_page;
 
-    function Paginator($total, $start)
+    function __construct($total, $start)
     {
         $this->total = $total;
         $this->start = $start;
@@ -20,7 +20,7 @@ class Paginator{
         $pag['total'] = $this->total;
         $pag['total_formatted'] = number_format($pag['total'], 0, ',', '.');
         $pag['start'] = $this->start;
-        $pag['total_pages'] = (($pag['total']/$this->count) % 10 == 0) ? (int)($pag['total']/$this->count) : (int)($pag['total']/$this->count+1);
+        $pag['total_pages'] = (($pag['total']/$this->count) % $this->count == 0) ? (int)($pag['total']/$this->count) : (int)($pag['total']/$this->count+1);
         $pag['count'] = $this->count;
 
         $range_min = (($this->current_page-5) > 0) ? $this->current_page-5 : 1;
