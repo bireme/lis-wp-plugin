b130f7660c161645ca3827371d68d98c0ee94bd0
gabrielszz <50212683+gabrielszz@users.noreply.github.com>
2026-04-08 13:59:02 -0300

    Correção mapa de evidências

--- template/lis-resource.php
+++ template/lis-resource.php
@@ -110,10 +110,26 @@ function temTableauNoLink($url) {
 
                         <div class='tableauPlaceholder' id='viz1769085678237' style='position: relative'><BR><noscript>
                         <a href='#'>
-                        <img alt='Estratégias de Promoção da Vacinação Infantil - Mapa de EvidênciasDECIT/SCTIE/MS; BIREME/OPAS/OMS ' 
+                        <img alt='' 
                         src='<?=$saida;?>' style='border: none' /></a>
-                        </noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='vacinas-pt/evidence-map' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='<?=$saida;?>' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='pt-BR' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1769085678237');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1050px';vizElement.style.height='1227px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
-                        
+                        </noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                        <param name='embed_code_version' value='3' /> <param name='site_root' value='' />
+                        <param name='name' value='vacinas-pt/evidence-map' /><param name='tabs' value='no' />
+                        <param name='toolbar' value='yes' /><param name='static_image' value='<?=$saida;?>' /> 
+                        <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' />
+                        <param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' />
+                        <param name='display_count' value='yes' /><param name='language' value='pt-BR' /></object>
+                        </div>        
+                    <script type='text/javascript'>                    var divElement = document.getElementById('viz1769085678237');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1050px';vizElement.style.height='1227px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+       
+                    <!---
+<div class='tableauPlaceholder' id='viz1775665260101' style='position: relative'><noscript>
+    <a href='#'><img alt='Mapa de Evidências da Efetividade Clínica da AromaterapiaBIREME&#47;OPAS&#47;OMS ' 
+    src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ar&#47;aromaterapia-pt&#47;evidence-map&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='aromaterapia-pt&#47;evidence-map' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ar&#47;aromaterapia-pt&#47;evidence-map&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='pt-BR' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1775665260101');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1016px';vizElement.style.height='1491px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+
+
+--->
+
                     <BR>
                         <?php } ?>
                     
